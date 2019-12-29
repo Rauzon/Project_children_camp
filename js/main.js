b458172d9d10df2fcd5b72bdcbd8ffb8440baf89
Crispyss <vladby86@gmail.com>
2019-12-29 18:59:25 +0300

    added slider in 'offer' block

---
+++ js/main.js
@@ -0,0 +1,7 @@
+    $('.offer_special-slider').slick({
+        speed : 1000,
+        infinite : true,
+        slidesToScroll: 1,
+        prevArrow: "<img src='/images/arrow-left-offer.png' class='prev' alt='1'>",
+        nextArrow: "<img src='/images/arrow_right_offer.png' class='next' alt='2'>",
+    });
